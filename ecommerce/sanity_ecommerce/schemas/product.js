7a25afd91c9a40391c1121adc7122c5c89bdfced
lucaspires-source <pires.lucas94@gmail.com>
2022-09-02 15:58:40 -0300

    conecting into schema

--- ecommerce/sanity_ecommerce/schemas/product.js
+++ ecommerce/sanity_ecommerce/schemas/product.js
@@ -29,7 +29,7 @@ export default {
         {
             name:'price',
             title:'Price',
-            type:'Number',
+            type:'number',
         },
         {
             name:'details',
