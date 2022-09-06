7e1ad7338c5d735ff726453d0b08edd40bdac11e
lucaspires-source <pires.lucas94@gmail.com>
2022-09-06 15:02:36 -0300

    basic draft of components

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,19 +1,19 @@
 import React from 'react'
+import Footer from '../components/Footer'
+import HeroBanner from '../components/HeroBanner'
 
 const Home = () => {
   return (
     <>
-    <div>
-      <h2>Best Selling Product</h2>
-      <p>Speakers of many variations</p>
-    </div>
-    <div>
-      {
-        ['Product1', 'Procuct2'].map(
-          (product) => product
-        )
-      }
-    </div>
+      <HeroBanner />
+      <div className="products-heading">
+        <h2>Best Selling Product</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className="products-container">
+        {['Product1', 'Procuct2'].map((product) => product)}
+      </div>
+      <Footer />
     </>
   )
 }
